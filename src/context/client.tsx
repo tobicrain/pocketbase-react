c1f974bfbad377d2c196a71f894309131d13e59a
tobicrain <tobikrain@gmail.com>
2022-10-10 18:57:39 +0200

    use normal pocketbase import

--- src/context/client.tsx
+++ src/context/client.tsx
@@ -1,14 +1,12 @@
 import * as React from 'react';
 import { createContext } from 'react';
-import { getPocketbase } from '../utils/Pocketbase';
+import PocketBase from 'pocketbase';
 
-const PocketBase = getPocketbase();
-
-export const ClientContext = createContext<typeof PocketBase | null>(null);
+export const ClientContext = createContext<PocketBase | null>(null);
 
 export type ClientProviderProps = {
   children: React.ReactNode;
-  client: typeof PocketBase;
+  client: PocketBase;
 };
 
 export const ClientProvider = (props: ClientProviderProps) => {
--- src/utils/Pocketbase.ts
+++
@@ -1,7 +0,0 @@
-export function getPocketbase() {
-    if (typeof document !== 'undefined') {
-        return require('pocketbase/dist/pocketbase.cjs.js');
-    } else if (typeof navigator !== 'undefined' && navigator.product === 'ReactNative') {
-        return require('pocketbase/dist/pocketbase.cjs');
-    }
-}
